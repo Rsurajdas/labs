05ddd219afeac2bb0ee17fae1b66adcba5ef5589
Suraj Kumar <suraj.k@pelorus.in>
2026-08-30 15:21:56 +0530

    add comments to clarify variable usage and shadowing in main function

--- rust/variables-and-mutability/src/main.rs
+++ rust/variables-and-mutability/src/main.rs
@@ -1,11 +1,13 @@
 fn main() {
     let age = 32;
 
-    let _fruit = 300;
+    let _fruit = 300; // Prefix an unused variable name with an underscore (`_`) to avoid unused-variable warnings.
 
     println!(
         "My age is {0}, and i am ready to be a awesome DAD! for my child.",
         age
     ); // In Rust's `println!` macro, positional arguments are explicitly indexed starting from `0`.
-    // println!("My age is {age}, and i am ready to be a awesome DAD! for my child.");
+
+    let x = "5";
+    let x = 5; // Variable shadowing allows you to redeclare a variable with the same name and assign it a new value.
 }
