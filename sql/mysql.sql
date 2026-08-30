7c9886fdd8336d4cff69f4af91a683692f649442
Suraj Kumar <suraj.k@pelorus.in>
2026-08-30 09:10:05 +0530

    INSERT statements in MySQL and PostgreSQL

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -1,7 +1,11 @@
 -- CREATE DATABASE IF NOT EXISTS talently;
 
-CREATE TABLE users(
-    full_name VARCHAR(100),
-    yearly_salary INT,
-    current_status ENUM("employed", "self-employed", "unemployed")
-)
+-- CREATE TABLE users(
+--     full_name VARCHAR(100),
+--     yearly_salary INT,
+--     current_status ENUM("employed", "self-employed", "unemployed")
+-- );
+
+-- INSERT INTO users (current_status, full_name, yearly_salary) VALUES ('employed', 'Suraj Kumar', 12473412);
+INSERT INTO users (current_status, full_name, yearly_salary) VALUES ('self-employed', 'Subhankar Kumar', 22473412);
+INSERT INTO users (current_status, full_name, yearly_salary) VALUES ('unemployed', 'abc', 0);
--- sql/postgres.sql
+++ sql/postgres.sql
@@ -2,8 +2,15 @@
 
 -- CREATE TYPE employment_status AS ENUM ('employed', 'self-employed', 'unemployed');
 
-CREATE TABLE users(
-    full_name VARCHAR(100),
-    yearly_salary INT,
-    current_status employment_status
-)
+-- CREATE TABLE users(
+--     full_name VARCHAR(100),
+--     yearly_salary INT,
+--     current_status employment_status
+-- );
+
+-- INSERT INTO users (current_status, full_name, yearly_salary) VALUES ('employed', 'Suraj Kumar', 12473412);
+-- INSERT INTO users (current_status, full_name, yearly_salary) VALUES ('self-employed', 'Subhankar Kumar', 22473412);
+-- INSERT INTO users (current_status, full_name, yearly_salary) VALUES ('unemployed', 'abc', 0);
+
+-- DELETE FROM users WHERE full_name = 'Subhankar Kumar';
+-- DELETE FROM users WHERE full_name = 'abc';
