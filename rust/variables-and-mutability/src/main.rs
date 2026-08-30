c9ab60e3923c86b3a3587cd90ab825125f46b584
Suraj Kumar <suraj.k@pelorus.in>
2026-08-30 15:25:09 +0530

    variable scope

--- rust/variables-and-mutability/src/main.rs
+++ rust/variables-and-mutability/src/main.rs
@@ -8,6 +8,11 @@ fn main() {
         age
     ); // In Rust's `println!` macro, positional arguments are explicitly indexed starting from `0`.
 
-    let x = "5";
-    let x = 5; // Variable shadowing allows you to redeclare a variable with the same name and assign it a new value.
+    let _x = "5";
+    let _x = 5; // Variable shadowing allows you to redeclare a variable with the same name and assign it a new value.
+
+    {
+        let age = 31;
+        println!("{}", age)
+    }
 }
