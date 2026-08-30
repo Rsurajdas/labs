0c8de4c760326f302d1ba6e24870e296c85c5b1d
Suraj Kumar <suraj.k@pelorus.in>
2026-08-30 09:53:58 +0530

    update println! macro to use positional arguments

--- rust/variables-and-mutability/src/main.rs
+++ rust/variables-and-mutability/src/main.rs
@@ -1,8 +1,11 @@
 fn main() {
     let age = 32;
 
+    let _fruit = 300;
+
     println!(
-        "My age is {}, and i am ready to be a awesome DAD! for my child.",
+        "My age is {0}, and i am ready to be a awesome DAD! for my child.",
         age
-    );
+    ); // In Rust's `println!` macro, positional arguments are explicitly indexed starting from `0`.
+    // println!("My age is {age}, and i am ready to be a awesome DAD! for my child.");
 }
