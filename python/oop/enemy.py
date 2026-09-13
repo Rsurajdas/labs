2943963c3153387028193daedfc65870b6eabff5
Suraj Kumar <suraj.k@pelorus.in>
2026-09-13 09:16:38 +0530

    refactor Enemy class: encapsulate attributes and update info, attack, and move methods

--- python/oop/enemy.py
+++ python/oop/enemy.py
@@ -1,14 +1,23 @@
 class Enemy:
     def __init__(self, type, health, damage):
-        self.type: str = type
-        self.health: int = health
-        self.damage: int = damage
+        self.__type: str = type
+        self.__health: int = health
+        self.__damage: int = damage
+
+    def get_type(self):
+        return self.__type
+
+    def get__health(self):
+        return self.__health
+
+    def get__damage(self):
+        return self.__damage
 
     def info(self):
-        return f"Type: {self.type}, Health: {self.health}, Damage: {self.damage}"
+        return f"Type: {self.__type}, Health: {self.__health}, Damage: {self.__damage}"
 
     def attack(self):
-        return f"{self.type} attacks for {self.damage} damage!"
+        return f"{self.__type} attacks for {self.__damage} damage!"
 
     def move_towards(self, target):
-        return f"{self.type} moves towards {target.type}!"
+        return f"{self.__type} moves towards {target.__type}!"
--- python/oop/main.py
+++ python/oop/main.py
@@ -3,6 +3,8 @@ from enemy import *
 zombie = Enemy("Zombie", 100, 5)
 orge = Enemy("Orge", 250, 15)
 
+# print(zombie.__type)  # This will raise an AttributeError because __type is private
+
 print(zombie.info())
 print(orge.info())
 
