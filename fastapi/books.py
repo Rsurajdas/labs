cb9ffe678356a544b882a4bf5235f1afc1ca3c78
Suraj Kumar <suraj.k@pelorus.in>
2026-09-13 18:07:49 +0530

    Refactor book endpoints for consistency and add update_book_by_id functionality

--- fastapi/books.py
+++ fastapi/books.py
@@ -4,30 +4,41 @@ from data import *
 app = FastAPI()
 
 @app.get("/books")
-async def get_all_books(in_stock: bool):
+async def get_all_books(in_stock: bool | None=None):
+    if in_stock == None:
+        return {"status": "success", "results": len(BOOKS), "data": BOOKS}
     result = []
     for book in BOOKS:
         if book.get("in_stock") == in_stock:
             result.append(book)
         
-    return {"status": "Success", "results": len(result), "data": result}
+    return {"status": "success", "results": len(result), "data": result}
 
 @app.get("/books/{id}")
 async def get_book_by_id(id: int):
     for book in BOOKS:
         if book.get("id") == id:
-            return {"status": "Success", "data": book}
+            return {"status": "success", "data": book}
         
-    return {"status": "Failed", "message": "Book not found!"}
+    return {"status": "failed", "message": "Book not found!"}
 
 @app.post("/books")
 async def create_book(book=Body()):
     if not book.get("id"):
-        return {"status": "Fialed", "message": "Books must have a unique `id`"}
+        return {"status": "failed", "message": "Books must have a unique `id`"}
     if not book.get("title"):
-        return {"status": "Fialed", "message": "Books must have a `title`"}
+        return {"status": "failed", "message": "Books must have a `title`"}
 
     BOOKS.append(book)
-    return {"status": "Success", "data": book, "message": "Book successfully added"}
+    return {"status": "success", "data": book, "message": "Book successfully added"}
+
+@app.put("/books/{id}")
+async def update_book_by_id(id: int, body=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get("id") == id:
+            BOOKS[i] = body
+            return {"status": "success", "message": "Book updated successfully"}
+        
+    return {"status": "failed", "message": "Book not found!"}
 
     
